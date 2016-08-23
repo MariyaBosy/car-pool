5d25f282369c9d619d252c7b52b3e3e14a0a63da
Prashant Baisla <prashant.baisla@practo.com>
2016-08-23 22:41:30 +0530

    Package as war instead of jar

--- src/main/java/com/practo/jedi/run/Application.java
+++ src/main/java/com/practo/jedi/run/Application.java
@@ -3,6 +3,8 @@ package com.practo.jedi.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,7 +14,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.practo.jedi")
 @EnableJpaRepositories("com.practo.jedi.data")
 @EntityScan("com.practo.jedi.data.entity")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
